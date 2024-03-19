d0525b1c1a4f2a439c71284d34694409df8e254e
Mickael Beaugrand <mickael.beaugrand90@gmail.com>
2024-03-19 12:16:24 +0100

    add chalk package

---
+++ index.js
@@ -0,0 +1,15 @@
+import chalk from 'chalk';
+
+console.log(
+    chalk.blue("Antoine")
+    );
+console.log(
+    chalk.yellow("Erwan")
+    );
+console.log(
+    chalk.red("Yazid")
+    );
+console.log(
+    chalk.green("Ludo")
+    );
+
